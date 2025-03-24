464fbeb9992cdcb6dd44eb47a52d901f17c504f5
Yoshiba54 <camille@agence-copernic.fr>
2025-03-24 11:57:08 +0100

    Récupération des franchises ok

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
+require("dotenv").config();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const HUBSPOT_BASE_URL = "https://api.hubspot.com/crm/v3/objects";
+const CUSTOM_OBJECT_ID = "2-140710940";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get("/", async (req, res) => {
+  console.log("Fetching franchises..." + PRIVATE_APP_ACCESS);
+  const franchises = `${HUBSPOT_BASE_URL}/${CUSTOM_OBJECT_ID}?properties=nom_de_la_franchise,ville,nombre_de_titres`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const resp = await axios.get(franchises, { headers });
+    const data = resp.data.results;
+    res.render("homepage", { title: "Franchises | HubSpot APIs", data });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -66,6 +84,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
